27e48e39df551d12b932423b1baa11188407ea35
muhammad ismail <muhismailmks@gmail.com>
2024-07-25 17:45:47 +0800

    update

--- assets/js/common.js
+++ assets/js/common.js
@@ -25,16 +25,16 @@ jQuery(document).ready(function ($) {
 
 	// Animation
 
-	function marqueeLinesChange(el) {
-		var point = $(window).scrollTop() + $(window).height() - el.offset().top;
-		var percent = point * 100 / ($(window).height() + el.outerHeight());
-		var translateX = percent / 5;
+			function marqueeLinesChange(el) {
+				var point = $(window).scrollTop() + $(window).height() - el.offset().top;
+				var percent = point * 100 / ($(window).height() + el.outerHeight());
+				var translateX = percent / 5;
 
-		if (translateX < 0) translateX = 0;
+				if (translateX < 0) translateX = 0;
 
-		el.find('.marquee-lines__wrap._left').css('transform', 'translate3d(-' + translateX + '%,0,0)');
-		el.find('.marquee-lines__wrap._right').css('transform', 'translate3d(' + translateX + '%,0,0)');
-	}
+				el.find('.marquee-lines__wrap._left').css('transform', 'translate3d(-' + translateX + '%,0,0)');
+				el.find('.marquee-lines__wrap._right').css('transform', 'translate3d(' + translateX + '%,0,0)');
+			}
 
 	$('.marquee-lines').each(function () {
 		var el = $(this);
